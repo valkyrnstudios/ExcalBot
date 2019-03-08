b9cd79deba14e0da4208b779114ccbde19a7672a
joel.garboden <joel@valkyrnstudios.net>
2019-03-07 22:53:35 -0800

    Automatically build talents list

--- cogs/specs.py
+++ cogs/specs.py
@@ -5,17 +5,36 @@ class Specs(commands.Cog, name='Spec commands'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True, aliases=['talents'])
+    @commands.command(name='talents')
+    async def talents(self, ctx):
+        parents = self.get_commands()
+        talents_list = []
+
+        for cmd in self.walk_commands():
+            if cmd not in parents:
+                #print (vars(cmd))
+                if cmd.name not in talents_list:
+                    talents_list.append(cmd.name)
+
+                for cmdalias in cmd.aliases:
+                    if cmdalias not in talents_list:
+                        talents_list.append(cmdalias)
+
+        talents_str = 'Available specs:\n* ' + '\n* '.join(talents_list)
+        await ctx.send(talents_str)
+
+    @commands.group(pass_context=True)
     async def spec(self, ctx):
         if ctx.invoked_subcommand is None:
-            try:
-                pass
-            except Exception as e:
-                await ctx.send('Unknown spec: {0.subcommand_passed}'.format(ctx))
+            await ctx.send('Unknown spec: {0.subcommand_passed}'.format(ctx))
 
-    @spec.command(name='coh')
+    @spec.command(name='coh', aliases=['mshate', 'cozzmo', 'pristsk'])
     async def _speccoh(self, ctx):
-        await ctx.send('Spec coh')
+        await ctx.send('Spec CoH')
+
+    @spec.command(name='enh', aliases=['enha'])
+    async def _specenh(self, ctx):
+        await ctx.send('Spec Enhancement')
 
 def setup(bot):
     bot.add_cog(Specs(bot))
