c6a49db851f45d0053c9246e874136d5aaf4610c
joel.garboden <joel@valkyrnstudios.net>
2019-03-08 15:13:13 -0800

    Clean up talents logic

--- cogs/specs.py
+++ cogs/specs.py
@@ -7,20 +7,17 @@ class Specs(commands.Cog, name='Spec commands'):
 
     @commands.command(name='talents')
     async def talents(self, ctx):
-        parents = self.get_commands()
         talents_list = []
-
+        parents = self.get_commands()
         for cmd in self.walk_commands():
             if cmd not in parents:
-                #print (vars(cmd))
-                if cmd.name not in talents_list:
-                    talents_list.append(cmd.name)
+                cmd_line_str = f'* **{cmd.name}**'
+                if len(cmd.aliases) > 0:
+                    cmd_line_str += ': [ ' + ' '.join(cmd.aliases) + ' ]'
+                if cmd_line_str not in talents_list:
+                    talents_list.append(cmd_line_str)
 
-                for cmdalias in cmd.aliases:
-                    if cmdalias not in talents_list:
-                        talents_list.append(cmdalias)
-
-        talents_str = 'Available specs:\n* ' + '\n* '.join(talents_list)
+        talents_str = 'Available specs:\n' + '\n'.join(talents_list)
         await ctx.send(talents_str)
 
     @commands.group(pass_context=True)
@@ -28,7 +25,7 @@ class Specs(commands.Cog, name='Spec commands'):
         if ctx.invoked_subcommand is None:
             await ctx.send('Unknown spec: {0.subcommand_passed}'.format(ctx))
 
-    @spec.command(name='coh', aliases=['mshate', 'cozzmo', 'pristsk'])
+    @spec.command(name='coh', aliases=['mshate', 'cozzmo', 'priestsk'])
     async def _speccoh(self, ctx):
         await ctx.send('Spec CoH')
 
